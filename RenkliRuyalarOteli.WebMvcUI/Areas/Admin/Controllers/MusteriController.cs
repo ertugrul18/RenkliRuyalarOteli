25abe1eeb5f0e5ecad67b254436d4e8219931d57
ertugrul18 <quars18@gmail.com>
2022-12-21 19:57:29 +0300

    .

--- RenkliRuyalarOteli.WebMvcUI/Areas/Admin/Controllers/MusteriController.cs
+++ RenkliRuyalarOteli.WebMvcUI/Areas/Admin/Controllers/MusteriController.cs
@@ -1,6 +1,10 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using RenkliRuyalarOteli.BL.Abstract;
+using RenkliRuyalarOteli.Entities.Entites.Abstract;
+using RenkliRuyalarOteli.Entities.Entites.Concrete;
+using RenkliRuyalarOteli.WebMvcUI.Areas.Admin.Models.Musteri;
+using System.Security.Claims;
 
 namespace RenkliRuyalarOteli.WebMvcUI.Areas.Admin.Controllers
 {
@@ -17,8 +21,90 @@ namespace RenkliRuyalarOteli.WebMvcUI.Areas.Admin.Controllers
         }
         public async Task<IActionResult> Index()
         {
+
             var result = await musteriManager.FindAllAsnyc();
             return View(result);
+        }
+
+
+        ////
+        ///
+
+        [HttpGet]
+        public async Task<IActionResult> Update(Guid Id)
+        {
+            var musteri = musteriManager.FindAsync(p => p.Id == Id).Result;
+            MusteriUpdateDTO updateDto = new MusteriUpdateDTO
+            {
+                Id = Id,
+                Ad = musteri.Ad,
+                Soyadi = musteri.Soyadi,
+                Cinsiyet = musteri.Cinsiyet,
+                MusteriTcNo = musteri.MusteriTcNo,
+                CepNo = musteri.CepNo,
+
+            };
+            return View(updateDto);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Update(MusteriUpdateDTO updateDTO)
+        {
+            var musteri = musteriManager.FindAsync(p => p.Id == updateDTO.Id).Result;
+            musteri.Ad = updateDTO.Ad;
+            musteri.Soyadi = updateDTO.Soyadi;
+            musteri.Cinsiyet = updateDTO.Cinsiyet;
+            musteri.MusteriTcNo = updateDTO.MusteriTcNo;
+            musteri.CepNo = updateDTO.CepNo;
+
+            musteri.UpdateDate = DateTime.Now;
+            musteri.Status = Status.Active;
+
+            var sonuc = await musteriManager.UpdateAsync(musteri);
+            if (sonuc > 0)
+            {
+                return RedirectToAction("Index", "Musteri");
+            }
+            else
+            {
+                ModelState.AddModelError("", "Bilinmeyen bir hata olustu. Lutfen Biraz sonra tekrar denbeyiniz");
+
+                return View(updateDTO);
+            }
+        }
+        [HttpGet]
+        public async Task<IActionResult> Create()
+        {
+            var createDto = new MusteriCreateDTO();
+            return View(createDto);
+        }
+        [HttpPost]
+        public async Task<IActionResult> Create(MusteriCreateDTO createDTO)
+        {
+            var musteri = new Musteri
+            {
+                Id = createDTO.Id,
+                Ad = createDTO.Ad,
+                Soyadi = createDTO.Soyadi,
+                Cinsiyet = createDTO.Cinsiyet,
+                MusteriTcNo = createDTO.MusteriTcNo,
+                CepNo = createDTO.CepNo,
+                KullaniciId = Guid.Parse(User.Claims.FirstOrDefault(p => p.Type == ClaimTypes.NameIdentifier).Value),
+                CreateDate = DateTime.Now,
+                Status = Status.Active
+            };
+            var sonuc = await musteriManager.CreateAsync(musteri);
+
+            if (sonuc > 0)
+            {
+                return RedirectToAction("Index", "Musteri");
+            }
+            else
+            {
+                ModelState.AddModelError("", "Bilinmeyen bir hata olustu. Daha Sonra Tekrar Deneyiniz");
+
+                return View(createDTO);
+            }
         }
     }
 }
---
+++ RenkliRuyalarOteli.WebMvcUI/Areas/Admin/Models/Musteri/MusteriUpdateDTO.cs
@@ -0,0 +1,17 @@
+﻿using RenkliRuyalarOteli.Entities.Entites.Concrete;
+
+namespace RenkliRuyalarOteli.WebMvcUI.Areas.Admin.Models.Musteri
+{
+    public class MusteriUpdateDTO
+    {
+        public Guid Id { get; set; }
+        public string Ad { get; set; }
+        public string Soyadi { get; set; }
+        public bool Cinsiyet { get; set; }
+        public string MusteriTcNo { get; set; }
+        public string CepNo { get; set; }
+
+        public Guid KullaniciId { get; set; }
+        public ICollection<Rezervasyon> Rezervasyonlari { get; set; }
+    }
+}
